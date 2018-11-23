1f595c66e35cf1e3290c8990135c9d76fdfc87ec
binbinyouli <binbinyouli@qq.com>
2018-11-23 16:57:32 +0800

    提交

--- filehadoop/src/main/java/com/huaren/hadoop/HadoopUtil.java
+++ filehadoop/src/main/java/com/huaren/hadoop/HadoopUtil.java
@@ -1,7 +1,28 @@
 package com.huaren.hadoop;
 
+import org.apache.commons.io.IOUtils;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataOutputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+
+
+import java.io.FileInputStream;
+
+
 /**
  * Created by yuantousanfen on 2018/11/23.
  */
 public class HadoopUtil {
+    public static void main(String[] args) throws Exception{
+       Configuration configuration=new Configuration();
+       configuration.set("fs.defaultFS","hdfs://Huaren6:9000");
+        System.out.println(configuration);
+        FileSystem fileSystem=FileSystem.get(configuration);
+        Path path= new Path("hdfs://Huaren6:9000/haha.txt");
+        //FSDataOutputStream outputStream=fileSystem.create(path);
+        /*FileInputStream inputStream= new FileInputStream("D:/qingshu.txt");
+        IOUtils.copy(inputStream, outputStream);*/
+
+    }
 }
